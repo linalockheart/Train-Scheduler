f30f50fca4b61a55b98d6e6c88e4a6d457a159b0
Lina Lockheart <angelinalockheart@gmail.com>
2018-06-28 15:48:03 -0400

    calculations working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -46,20 +46,16 @@ function clock() {
         firstTrain = $("#first-train-input").val().trim();
         frequency = $("#frequency-input").val().trim();
 
-//use .push instead of .set to stop overwriting the info in firebase
-        var newTrain = {
+        database.ref().push({
             trainName: trainName, 
             destination: destination,
             firstTrain: firstTrain,
             frequency: frequency
-        };
-
-        database.ref().push(newTrain);
-
-        console.log(newTrain.trainName);
-        console.log(newTrain.destination);
-        console.log(newTrain.firstTrain);
-        console.log(newTrain.frequency);
+        })
+        // console.log(trainName);
+        // console.log(destination);
+        // console.log(firstTrain);
+        // console.log(frequency);
 
         $("#train-name-input").val("");
         $("#destination-input").val("")
@@ -69,9 +65,32 @@ function clock() {
       });
 
     database.ref().on("child_added", function(childSnapshot) {
-        console.log(childSnapshot);
+        // console.log(childSnapshot.val().trainName);
+        // console.log(childSnapshot.val().destination);
+        // console.log(childSnapshot.val().firstTrain);
+        // console.log(childSnapshot.val().frequency);
 
-        var currentTime = moment();
+        console.log("first train before converting: " + childSnapshot.val().firstTrain);
+
+        var currentTime = moment(currentTime).format("HH:mm");
+        // console.log("current time: " + currentTime);
+
+        var firstTrainTime = moment(childSnapshot.val().firstTrain, "HH:mm").subtract(1, "years");
+        console.log("first train converted " + firstTrainTime);
+
+        var timeDiff = moment().diff(moment(firstTrainTime), "minutes");
+        console.log("time diff " + timeDiff);
+
+        var timeApart = timeDiff % parseInt(childSnapshot.val().frequency);
+        console.log("time apart " + timeApart);
+
+        // Minute Until Train
+        var minAway = parseInt(childSnapshot.val().frequency) - timeApart;
+        console.log("minutes away " + minAway);
+
+        // Next Train
+        var nextTrain = moment().add(minAway, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
 
         var tBody = $("tbody");
         var tRow = $("<tr>");
@@ -79,8 +98,8 @@ function clock() {
         var trainNameTD = $("<td>").text(childSnapshot.val().trainName);
         var destinationTD = $("<td>").text(childSnapshot.val().destination);
         var frequencyTD = $("<td>").text(childSnapshot.val().frequency);
-        var nextTrainTD = $("<td>") //this needs to be next train instead of first train
-        var minAwayTD = $("<td>"); // need to add something here to calculate
+        var nextTrainTD = $("<td>").text((nextTrain).format("HH:mm"));
+        var minAwayTD = $("<td>").text(minAway);
 
         tRow.append(trainNameTD, destinationTD, frequencyTD, nextTrainTD, minAwayTD);
         tBody.append(tRow);
@@ -88,13 +107,6 @@ function clock() {
         }, function(errorObject) {
         console.log("The read failed: " + errorObject.code);
         });
-
-        // $("#clear-form").on("click", function(clear) {
-        //     $("#train-name-input").empty();
-        //     $("#destination-input").empty();
-        //     $("#first-train-input").empty();
-        //     $("#frequency-input").empty();
-        // })
 
 
     // Call Functions
